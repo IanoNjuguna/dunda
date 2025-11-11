64f906c8a8c6119225012ae2bb24852835dbcf92
IanoNjuguna <inc3099@gmail.com>
2025-11-11 21:49:34 +0300

    Revert "connect and dashboard sections"
    This reverts commit 305d6d1564bb0cbb036ae15490650bdadd5facef.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import MintForm  from './components/Dashboard'
-import ConnectWalletSection from './components/ConnectWalletSection'
 
 function App() {
+  const [count, setCount] = useState(0)
   // track wallet address and provide a mint success handler required by MintForm
   const [walletAddress, setWalletAddress] = useState<string>('')
 
@@ -13,10 +15,11 @@ function App() {
   }
 
   return (
-    <div>
+    <>
+    
+      
       <MintForm walletAddress={walletAddress} onMintSuccess={handleMintSuccess} />
-      <ConnectWalletSection {...({ setWalletAddress } as any)} />
-    </div>
+    </>
   )
 }
 
